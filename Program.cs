3aac66508b9c40247955f0dc19325ee8e0db409a
SlaVaXX <svyatoslav.solianyk@gmail.com>
2023-02-23 00:03:29 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.WriteLine("Введіть довжину вектора:");
+            string temp = Console.ReadLine();
+            int length = Convert.ToInt32(temp);
+            Console.WriteLine("Введіть кут під яким знаходиться вектор:");
+            temp = Console.ReadLine();
+            int angle = Convert.ToInt32(temp);
+            Vector A = new Vector(angle, length);
+            Console.WriteLine("Координати вектора:");
+            Console.WriteLine(A.Coordinates);
+        }
+
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Vector
+    {
+        private double angle;
+        private double length;
+        private Vector2 coordinates;
+        public Vector (double angle, double length)
+        { 
+            this.angle = angle;
+            if (length < 0)
+                Math.Abs(length);  
+            this.length = length;
+            coordinates = set_coordinates(angle, length);
+        }
+        public Vector2 set_coordinates (double angle, double length)
+        {
+            double x = length * Math.Cos(angle);
+            double y = length * Math.Sin(angle);
+            Vector2 result = new Vector2((float)x,(float)y);
+            return result;
+        }
+        public Vector2 Coordinates
+        {
+            get { return coordinates; }
+        }
+        ~Vector()
+        {
+            Console.WriteLine("The end");
+        }
+    }
+}
